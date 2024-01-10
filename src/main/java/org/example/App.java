eca5fbb4f10c1a0c71e08989ce9005426ceded06
pejko <aleksandarpejkovic@hotmail.com>
2024-01-10 21:09:39 +0100

    Convert existing application to be Spring boot Application

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,10 +1,19 @@
 package org.example;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
+@SpringBootApplication
 class App {
+
+    protected App() {
+    }
+
     public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
         log.info("Rest task application is up and running");
     }
 }
